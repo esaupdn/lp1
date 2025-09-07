6ba63c56e69dceb24052531c9efee48e55f526d9
Esaú Paiva <esaupaivanascimento@gmail.com>
2025-09-07 18:44:42 -0300

    atividade

---
+++ Atividade3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string mensagemCodificada = Console.ReadLine();
+        string mensagemDecodificada = Decodificar(mensagemCodificada);
+        Console.WriteLine(mensagemDecodificada);
+    }
+    static string Decodificar(string mensagem)
+    {
+        string resultado = "";
+        int i = 0;
+        while (i < mensagem.Length)
+        {      
+            if (mensagem[i] == 'p' || mensagem[i] == 'P')
+            {
+                i++;
+                if (i < mensagem.Length)
+                {
+                    resultado += mensagem[i];
+                    i++;
+                }
+            }
+            else
+            {     
+                resultado += mensagem[i];
+                i++;
+            }
+        }
+        return resultado;
+    }
+}
